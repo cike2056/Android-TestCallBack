751a3bef2df41e3e132e59f200535765adfb0c20
cike2056 <whzhn1990@163.com>
2014-12-03 20:11:05 +0800

    iCallBack.execute(jsonObject);/**如果把这部分去掉,可以运行.但是没有获得结果.

--- src/com/qf/android_testcallback/activity/MainActivity.java
+++ src/com/qf/android_testcallback/activity/MainActivity.java
@@ -9,7 +9,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
+/**接口回调/主线程调用子线程的方法 /handler
+ * 几点问题:
+ * @author uaige
+ *
+ */
+/*可以正常运行:
+	1.解决:空指针问题 public void getURLJSONContent(final String url,final ICallBack  callBack)中要赋值iCallBack =callBack;
+	2.解决:url的参数不符问题.String中多了一个空格.
+	3.
+private Handler handler = new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			JSONObject jsonObject = (JSONObject) msg.obj;
+			iCallBack.execute(jsonObject);*//**如果把这部分去掉,可以运行.但是,没有获得结果.*//*
+		}
+	};*/
 public class MainActivity extends Activity {
 
 	@Override
